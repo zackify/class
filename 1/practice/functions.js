5e5c0db59c5444a2cab9f2ea1ec80c2137f46ea3
Zach <zackify@gmail.com>
2018-07-21 21:31:31 -0700

    Another function

--- 1/practice/functions.js
+++ 1/practice/functions.js
@@ -9,3 +9,14 @@ function anotherSum(x, y) {
 const inlineSum = (x, y) => x + y;
 
 const addTwo = x => x * 2;
+
+//Realistic example of a small function
+//url slug
+
+let post = 'How to be cool like Alex';
+
+let toSlug = name => name.toLowerCase().replace(' ', '-');
+
+let slug = toSlug(post);
+
+console.log(slug);
