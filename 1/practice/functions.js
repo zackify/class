beb7fdcb56642963558d8c0b2a12419a1c28b57e
Zach <zackify@gmail.com>
2018-07-21 21:44:58 -0700

    Advanced callback function

--- 1/practice/functions.js
+++ 1/practice/functions.js
@@ -19,4 +19,34 @@ let toSlug = name => name.toLowerCase().replace(' ', '-');
 
 let slug = toSlug(post);
 
-console.log(slug);
+console.log(slug); //does this work?
+
+//Functions in functions
+
+addTwo(inlineSum(2, 2));
+
+//ok if we can do that... what about allowing custom functions?
+
+/* 
+  Pretend we have a function that formats names.
+  and it lets us customize the name even more if we want
+*/
+
+const renderNames = callback => {
+  //pretend this is loaded from a server
+  let names = ['ZACHHHH', 'FUCK ALEX', 'FUCK MKG', 'Sam'];
+
+  return names.map(name => {
+    let modifiedName = name.toLowerCase();
+    return callback(modifiedName);
+  });
+};
+
+//why could we use names again?
+
+let names = renderNames(name => {
+  if (name.match(/fuck/)) return '****';
+  return name;
+});
+
+console.log(names);
