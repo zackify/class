08fb89bc30555862cb49e5d62504dbbd6acc44d3
rodneystrong <akang2@gmail.com>
2018-07-23 21:17:00 -0700

    got the 1st quiz working

--- edits/calc.js
+++ edits/calc.js
@@ -10,3 +10,19 @@ c('lel');
 let addNUms = (number1, number2) => number1 + number2;
 
 c(addNUms(43, 13));
+
+//learnin about objects
+let car = {
+  name: 'dodge',
+  tires: 4,
+  color: 'black',
+  getMiles(miles) {
+    c(miles);
+    return miles;
+  }
+}
+
+car.getMiles(341);
+
+//your car is a name it has x tires, and x miles
+c(`Your car is a ${car.name} it has ${car.tires} tires, and ${car.getMiles(344141)} miles`);
