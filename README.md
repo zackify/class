2f952fa70f66ac2e24a00735a06f62082080836c
Zach <zackify@gmail.com>
2018-07-23 17:29:29 -0700

    Add some work stuff

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Setup
 
 - Download latest Node (v10) https://nodejs.org/en/
 - Download VSCode https://code.visualstudio.com/
+- Signup for github and fork this repository
---
+++ work/2-array.js
@@ -0,0 +1,10 @@
+//Loop over this array and return an error message to the console each time a blog post in this array is null
+
+let posts = [
+  'How to code',
+  null,
+  'I am a pro coder',
+  null,
+  null,
+  'I love to blog',
+];
