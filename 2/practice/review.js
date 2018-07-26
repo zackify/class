69969aa25b03f28b6e5c3d39edec7abb250180a0
Zach <zackify@gmail.com>
2018-07-25 20:31:12 -0700

    Add person test

--- 2/practice/review.js
+++ 2/practice/review.js
@@ -1,22 +1,24 @@
-let nameTest = () => {
-  let name = 'John';
+// let nameTest = name => {
 
-  let line = `Hello there ${name}.`;
-  console.log(line);
-};
+//   let line = `Hello there ${name}.`;
+//   console.log(line);
+// };
 
 //Why doesn't this run ^
+//console.log(line);
 
-
-
-
-const person = {
-  name: 'Zach'
-  about: 'Codes for dayz bruh'
-}
+let person = {
+  name: 'Zach',
+  about: 'Codes for dayz bruh',
+};
 
 // whats wrong ^
 
+//Log out: "Name is Zach, he Codes for dayz bruh"
 
-//Log out: "Name: {X}, like to {about}"
-console.log()
+let test = () => {
+  let blah = true;
+  return blah;
+};
+
+console.log(`Name is ${person.name} he ${person.about}`);
