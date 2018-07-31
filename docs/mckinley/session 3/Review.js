6b45bf90c7c207a1bc6e5769cb4009f74abb41d9
Mckinley <mckinleygilmore@gmail.com>
2018-07-30 20:03:52 -0700

    hello gov

---
+++ docs/mckinley/session 3/Review.js
@@ -0,0 +1,31 @@
+/*
+  Make an object with 2 properties: firstName, lastName, and getName
+*/
+
+
+
+/*
+  make a function called getGreeting 
+  that takes a first name, and last name
+
+  and returns the name joined together with a greeting message, ex:
+  Hello, First, Last
+*/
+
+
+
+/*
+
+Refactor function to take a single argument with zach
+
+*/
+
+
+let person = {
+  name: `Forest`
+  last name: `Brown`
+
+};
+getGreeting
+getName = (x) => {}
+console.log
