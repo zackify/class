b53bf5df9f1499db6ea33950d56b199edf733629
sam <z@zs-macbook.lan>
2018-07-30 19:32:48 -0700

    session 2 work

---
+++ docs/sam/2/loops.js
@@ -0,0 +1,25 @@
+// let start = 0
+// let end = 12
+// while(start <= end) {
+
+// console.log(start)
+// start = start + 3;
+// }
+
+
+let start = 10
+let end = 5
+let greetings = {
+    first: 'Hello',
+    anytime: "What's up",
+    end: `Goodbye`,
+}
+
+while (start * 2 > end) {
+start--;
+
+if (start === 10){
+
+console.log(greetings.first);
+}
+}
