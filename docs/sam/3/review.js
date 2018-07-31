b9397609b648f5c0a85a32effb95a892d63f068a
sam <samhuntress@rocketmail.com>
2018-07-30 19:51:54 -0700

    review attempt

---
+++ docs/sam/3/review.js
@@ -0,0 +1,24 @@
+/*
+  Make an object with 3 properties: firstName, lastName
+*/
+let object = {
+  firstName: 'Sam',
+  lastName: 'Huntress',
+};
+
+
+/*
+  make a function called getGreeting 
+  that takes a first name, and last name
+
+  and returns the name joined together with a greeting message, ex:
+  Hello, First, Last
+*/
+console.log(`Hello, ${object.firstName} ${object.lastName}`);
+
+
+/*
+
+Refactor function to take a single argument with zach
+
+*/
