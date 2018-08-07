07a6ad8d58f1053564c10195f7f2b1564435b1a9
Zach <zackify@gmail.com>
2018-08-06 21:20:38 -0700

    Add all work

---
+++ sessions/4/practice/foreach.js
@@ -0,0 +1,23 @@
+let posts = [
+  {
+    title: 'Test post',
+    description: 'Mckinley loves to make objects about ponies',
+    date: `August 27th, 2018`,
+  },
+  {
+    title: 'Test two',
+    description: 'Alex loves to make objects about ponies n bronies',
+    date: `August 27th, 2018`,
+  },
+  {
+    title: 'Test three',
+    description: 'Alex loves to make objects about ponies n bronies',
+    date: `August 27th, 2018`,
+  },
+];
+
+// let each = post => console.log(post);
+
+// posts.forEach(each);
+
+posts.forEach(post => console.log(post));
--- sessions/4/review.js
+++ sessions/4/review.js
@@ -2,7 +2,20 @@
   title, description, date
 */
 
+let post = {
+  title: 'Test post',
+  description: 'Mckinley loves to make objects about ponies',
+  date: `August 27th, 2018`,
+};
+
 /*
 
 make a while loop that puts the title 5 times in the console
 */
+
+let start = 0;
+
+while (start < 5) {
+  start++;
+  console.log(post.title);
+}
