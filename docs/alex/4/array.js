a888afd2987be8a55a91a1fc54a47e7741bc1322
rodneystrong <akang2@gmail.com>
2018-08-06 20:33:53 -0700

    looping through arrays

--- docs/alex/4/array.js
+++ docs/alex/4/array.js
@@ -1,5 +1,37 @@
-let post = {
+//shortcut for console
+function c(l) {
+  console.log(l);
+}
+
+let post1 = {
   title: 'Test post',
   description: 'Mickinley loves to make objects about ponies',
   date: `August 27th, 2018`,
 };
+
+let post2 = {
+  title: 'Test post',
+  description: 'Alex loves to make objects about bronies n ponies',
+  date: `August 27th, 2018`,
+};
+
+let post3 = {
+  title: 'Test post',
+  description: 'Alex loves to make objects about bronies n ponies',
+  date: `August 27th, 2018`,
+};
+
+let postsObject = {
+  post1: post1,
+  post2: post2,
+};
+
+let posts = [post1, post2, post3];
+
+c(posts.length);
+
+console.log(posts);
+
+for (let i = 0; i < posts.length; i++) {
+  c(i + ' hello');
+}
