101f6ec6acdd8e22732273254620262c0e97e9b7
Zach Silveira <zackify@gmail.com>
2018-08-13 17:05:49 -0700

    Get class 5 ready... ish

---
+++ sessions/5/review/index.js
@@ -0,0 +1,11 @@
+/*
+  create an array of posts, posts need to have at least 3 fields on them
+*/
+
+/*
+ loop through the array, and console.log a field (key) on it each time
+*/
+
+/*
+ make an array of numbers and add them together using reduce
+*/
